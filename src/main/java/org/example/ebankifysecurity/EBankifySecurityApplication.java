007ac262ade291928903354ace682e840ee8935b
MedELHattab <136116042+MedELHattab@users.noreply.github.com>
2024-12-02 12:17:01 +0100

    Initial commit

---
+++ src/main/java/org/example/ebankifysecurity/EBankifySecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.ebankifysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankifySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBankifySecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/ebankifysecurity/EBankifySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ebankifysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankifySecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
